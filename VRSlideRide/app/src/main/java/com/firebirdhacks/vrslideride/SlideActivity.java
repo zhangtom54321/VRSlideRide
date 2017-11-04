1db7977ccab3d6586d70725a2016286d00b84ef4
Gautham Raghupathi <gauthamar11@gmail.com>
2017-11-04 11:51:42 -0700

    slide activity changed

--- VRSlideRide/app/src/main/java/com/firebirdhacks/vrslideride/SlideActivity.java
+++
@@ -1,8 +0,0 @@
-package com.firebirdhacks.vrslideride;
-
-/**
- * Created by Gautham on 11/4/2017.
- */
-
-public class SlideActivity {
-}
---
+++ VRSlideRide/app/src/main/java/com/firebirdhacks/vrslideride/SlideView.java
@@ -0,0 +1,13 @@
+package com.firebirdhacks.vrslideride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlideView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_view);
+    }
+}
